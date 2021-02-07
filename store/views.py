9fa6e85a61c77bfbcaa9768bc5a4b93c29e289e5
Ajakaye <mailpaulade@gmail.com>
2021-02-07 17:06:25 +0100

    completed

--- store/views.py
+++ store/views.py
@@ -7,6 +7,14 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Product
+from django.conf import settings
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
+from django.core.cache import cache
+
+
+
+
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
 # Create your views here.
 
@@ -32,5 +40,5 @@ def view_cached_books(request):
         products = Product.objects.all()
         results = [product.to_json() for product in products]
         # store data in cache
-        cache.set(product, results, timeout=CACHE_TTL)
+        cache.set(products, results, timeout=CACHE_TTL)
         return Response(results, status=status.HTTP_201_CREATED)
