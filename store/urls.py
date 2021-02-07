ca42421500ce1c00b8dd779c69771d1adf0a9e75
Ajakaye <mailpaulade@gmail.com>
2021-02-07 16:38:50 +0100

    added cache view

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from .views import view_books
+from .views import view_books, view_cached_books
 
 
 urlpatterns = [
-    path('',view_books)
+    path('',view_books),
+    path('cache/', view_cached_books),
+
 ]
 
 
--- store/views.py
+++ store/views.py
@@ -18,3 +18,19 @@ def view_books(request):
     products = Product.objects.all()
     results = [product.to_json() for product in products]
     return Response(results, status=status.HTTP_201_CREATED)
+
+
+
+@api_view(['GET'])
+def view_cached_books(request):
+    if 'product' in cache:
+        # get results from cache
+        products = cache.get('product')
+        return Response(products, status=status.HTTP_201_CREATED)
+
+    else:
+        products = Product.objects.all()
+        results = [product.to_json() for product in products]
+        # store data in cache
+        cache.set(product, results, timeout=CACHE_TTL)
+        return Response(results, status=status.HTTP_201_CREATED)
